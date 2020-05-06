714feaed79431b0302de9e07028b7bf77ee3aa17
Nway Sandi Oo <e0395885@u.nus.edu>
2020-05-06 17:28:42 +0800

    Update and Create Restaurants in memory

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -10,6 +10,11 @@ namespace OdeToFood.Data
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int id);
+
+        Restaurant Update(Restaurant updatedRestaurant);
+
+        Restaurant Add(Restaurant newRestaurant);
+        int Commit();
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -32,10 +37,35 @@ namespace OdeToFood.Data
             return restaurants.SingleOrDefault(r => r.Id== id);
         }
 
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if (restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cusine = updatedRestaurant.Cusine;
+            }
+            return restaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+
         public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
         {
             return restaurants.Where(r=> string.IsNullOrEmpty(name) || r.Name.StartsWith(name)).OrderBy(r=>r.Name);
             
         }
+
     }
 }
---
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData restaurnatData;
+        private readonly IHtmlHelper htmlHelper;
+
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        public EditModel(IRestaurantData restaurantData,
+                         IHtmlHelper htmlHelper)
+        {
+            this.restaurnatData = restaurantData;
+            this.htmlHelper = htmlHelper;
+        }
+        public IActionResult OnGet(int? restaurantId)
+        {
+            Cuisines = htmlHelper.GetEnumSelectList<CusineType>();
+            if (restaurantId.HasValue)
+            {
+                Restaurant = restaurnatData.GetById(restaurantId.Value);
+            }
+            else
+            {
+                Restaurant = new Restaurant();
+            }
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                Cuisines = htmlHelper.GetEnumSelectList<CusineType>();
+                return Page();
+                
+            }
+
+            if (Restaurant.Id > 0)
+            {
+                restaurnatData.Update(Restaurant);
+            }
+            else
+            {
+                restaurnatData.Add(Restaurant);
+            }
+            restaurnatData.Commit();
+            TempData["Message"] = "Restaurant Save!";
+            return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
+
+        }
+    }
+}
